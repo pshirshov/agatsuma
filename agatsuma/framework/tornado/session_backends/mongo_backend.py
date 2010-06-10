fb7d3250f3908f1029132db688e2b9f95abd2204
Fei Wong Reed <feiwreed@gmail.com>
2010-06-11 00:26:51 +0700

    missing file

---
+++ agatsuma/framework/tornado/session_backends/mongo_backend.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+import pymongo
+import re
+import time
+import datetime
+
+from agatsuma.log import log
+from agatsuma.interfaces import AbstractSpell
+from agatsuma.framework.tornado import SessionBackendSpell
+from agatsuma.framework.tornado import BaseSessionManager
+
+
+"""
+Used code from
+http://github.com/milancermak/tornado/blob/master/tornado/session.py
+"""
+
+
+"""
+class AutoReconnect(object):
+    functions = {}
+    def __init__(self, method):
+        self.method = method
+
+    def __call__(self, *args, **kwargs):
+"""    
+
+class MongoSessionManager(BaseSessionManager):
+    def __init__(self, uri):
+        BaseSessionManager.__init__(self)
+        self.uri = uri
+        self.initConnection()
+
+    def initConnection(self):
+        log.sessions.info("Initializing MongoDB session backend using URI '%s'" % self.uri)
+        connData = self._parse_connection_details(self.uri)
+        self.connection = pymongo.Connection(connData[0], int(connData[1]))
+        self.dbSet = self.connection[connData[2]]
+        self.db = self.dbSet.sessions
+        
+    @staticmethod
+    def _parse_connection_details(details):
+        # mongodb://[host[:port]]/db
+        match = re.match('mongodb://([\S|\.]+?)?(?::(\d+))?/(\S+)', details)
+        return match.group(1), match.group(2), match.group(3) # host, port, database
+
+    def cleanup(self):
+        self.db.remove({'expires': {'$lte': int(time.time())}})
+
+    def destroyData(self, session_id):
+        try:
+            self.db.remove({'session_id': session_id})
+            self.connection.end_request()
+        except pymongo.errors.AutoReconnect:
+            log.sessions.critical("Mongo exception during destroying %s" % session_id)
+
+    def loadData(self, session_id):
+        try:
+            data = self.db.find_one({'session_id': session_id})
+            self.connection.end_request()
+            if data:
+                return data["data"]
+        except pymongo.errors.AutoReconnect:
+            log.sessions.critical("Mongo exception during loading %s" % session_id)
+        except Exception, e:
+            log.sessions.critical("Unknown exception during loading %s" % str(e))
+            self.connection.end_request()
+
+    def saveData(self, session_id, data):
+        expTime = int(time.mktime(self._sessionDoomsday(datetime.datetime.now()).timetuple()))
+        try:
+            self.db.update(
+                {'session_id': session_id}, # equality criteria
+                {'session_id': session_id,
+                'data': data,
+                'expires': expTime,
+                }, # new document
+                upsert=True)
+            self.connection.end_request()
+        except pymongo.errors.AutoReconnect:
+            log.sessions.critical("Mongo exception during saving %s with data %s" % (session_id, str(data)))
+            
+class MongoSessionSpell(AbstractSpell, SessionBackendSpell):
+    def __init__(self):
+        config = {'info' : 'MongoDB session storage',
+                  'deps' : ()
+                 }
+        AbstractSpell.__init__(self, 'tornado_session_backend_mongodb', config)
+        
+    def instantiateBackend(self, uri):
+        self.managerInstance = MongoSessionManager(uri)
+        return self.managerInstance
+
+    def preConfigure(self, core):
+        core.registerEntryPoint("mongodb:sessions:cleanup", self.entryPoint)
+        
+    def entryPoint(self, argv):
+        log.core.info("Cleaning old sessions in MongoDB")
+        self.managerInstance.cleanup()
