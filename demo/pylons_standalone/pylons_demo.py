247d93824aa92bcde082939fbf837009ff23a07e
Fei Wong Reed <feiwreed@gmail.com>
2010-07-15 23:21:10 +0700

    cleanups

--- demo/pylons_standalone/pylons_demo.py
+++ demo/pylons_standalone/pylons_demo.py
@@ -17,6 +17,8 @@ class AppGlobals(BaseGlobals):
 def make_core(*args, **kwargs):
     return PylonsCore([(appRoot, "pylons_standalone")], *args, **kwargs)
 
+# Entry points are provided by spell in appspell.py
+# Don't forget to generate egg info before running paster with "./setup.py egg_info"
 def make_app(global_conf, full_stack=True, static_files=True, **app_conf):
     from pylons_standalone.lib import helpers
     core = make_core(appConfig, helpers = helpers,
