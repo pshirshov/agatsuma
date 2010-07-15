b05fba7e29a78b347a91e430b6bf1220dbf1d050
Fei Wong Reed <feiwreed@gmail.com>
2010-07-15 23:25:05 +0700

    comments

--- demo/pylons_standalone/pylons_demo.py
+++ demo/pylons_standalone/pylons_demo.py
@@ -14,11 +14,16 @@ class AppGlobals(BaseGlobals):
     def __init__(self, config):
         BaseGlobals.__init__(self, config)
 
+# Yeah, this function and all the setup.py code looks like
+# small piece of shit. I'm sorry, but pylons and paster are so overbloated.
+# I've tried to beautifize pylons initialization as much as possible
+# But it is still not so good...
 def make_core(*args, **kwargs):
     return PylonsCore([(appRoot, "pylons_standalone")], *args, **kwargs)
 
 # Entry points are provided by spell in appspell.py
 # Don't forget to generate egg info before running paster with "./setup.py egg_info"
+# Don't change signature, paster relies on it
 def make_app(global_conf, full_stack=True, static_files=True, **app_conf):
     from pylons_standalone.lib import helpers
     core = make_core(appConfig, helpers = helpers,
